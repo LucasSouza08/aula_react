19921c625b6686bef1b1d876879678ad7261eabc
LucasSouza08 <lucas.marcelino0403@gmail.com>
2026-04-22 14:57:20 -0300

    22/04

---
+++ aula01/src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css"
+
+export default function App() 
+
+{
+  let nome, idade, altura
+
+  nome="Lucas";
+  idade= 16;
+  altura= 1.81;
+  dias= idade*365
+  return (
+
+    <div>
+      <h1>Olá Mundo !!!</h1>
+
+
+      <div className="conteudo">
+
+        <p>Olá, seja bem-vindo ao React com JS.</p>
+
+        <p>Olá </p>
+
+      </div>
+    </div>
+
+
+  )
+}
