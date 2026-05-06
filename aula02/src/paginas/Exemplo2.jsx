2e62d4525c575980ce5d5a14170646d5b50fd2df
LucasSouza08 <lucas.marcelino0403@gmail.com>
2026-05-06 14:30:07 -0300

    06/05

--- aula02/src/paginas/Exemplo2.jsx
+++ aula02/src/paginas/Exemplo2.jsx
@@ -1,4 +1,4 @@
-export default function App ()
+export default function AExemplo2 ()
 {
   return (
     <div>
--- aula02/src/paginas/Exercicio2.jsx
+++ aula02/src/paginas/Exercicio2.jsx
@@ -1,4 +1,4 @@
-export default function App ()
+export default function Exercicio2 ()
 {
   return (
     <div>
